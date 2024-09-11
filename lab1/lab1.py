7af636f28095f1fe52c726ad6e0ba5d08981766c
Keshav <keshav.23fe10cse00589@muj.manipal.edu>
2024-09-11 10:10:21 +0530

    add lab1 21Aug

---
+++ lab1/lab1.py
@@ -0,0 +1,21 @@
+msg = 'hello py'
+print(msg)
+msg = "hello py"
+print(msg)
+
+name = 'Keshav Kumar'
+print(name)
+first_name = 'Keshav'
+last_name = 'Kumar'
+print(first_name)
+print(last_name)
+full_name = f'{first_name} {last_name}'
+print(full_name)
+
+name = 'keshav kumar'
+print()
+print(name)
+print(name.title())
+print()
+
+print(f'Hello, {name.title()}!')
---
+++ lab1/lab1_2.py
@@ -0,0 +1,21 @@
+name = input('Enter your name: ')
+print(name.title())
+
+fName = input('first ame: ')
+lName = input('last name: ')
+print(f'Your full name is {fName.title()} {lName.title()}')
+
+a = input('any numba: ')
+print(a)
+print(type(a))
+b = input('anotha numba: ')
+print(b)
+c = a + b
+print(c)
+
+a = int(input('any numba: '))
+print(type(a))
+b = int(input('anotha numba: '))
+print(b)
+c = a + b
+print(c)
